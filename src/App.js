b234906f01727f12d87c909e9f16c433772f169b
Zeferu2 <zeferu19@gmail.com>
2024-11-30 15:42:46 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import LabTest from './component/LabTest';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to LabTest App</h1>
+      <LabTest /> 
+    </div>
+  );
+}
+
+export default App;
